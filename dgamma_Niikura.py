821cb3d0bbfd5fe720f28a872a2149a8c0d825ba
gortonm <matthew.gorton@nottingham.ac.uk>
2022-05-12 17:49:22 +0100

    Include separate event rates from MW and M31.

--- dgamma_Niikura.py
+++ dgamma_Niikura.py
@@ -11,33 +11,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 f_pbh = 1
+u_0 = 1e-5
 u_T = 1  # threshold impact parameter, chosen to reproduce Fig. 8
 
 def v_r(x, u_min, m_pbh, t_hat):
     return 2 * einstein_radius(x, m_pbh) * np.sqrt(u_T - u_min**2)
 
-def dgamma_integrand(x, u_min, args=(m_pbh, t_hat)):
+def dgamma_integrand_MW(x, u_min, args=(m_pbh, t_hat)):
+    prefactor = 2 * f_pbh * d_s / (m_pbh)
+    first_term = rho_MW(x) * v_r(x, u_min, m_pbh, t_hat)**4 * np.exp(-(v_r(x, u_min, m_pbh, t_hat) / v_0_MW)**2) / (v_0_MW **2 * np.sqrt(u_T - u_min**2))
+    return prefactor * first_term
+
+def dgamma_integrand_M31(x, u_min, args=(m_pbh, t_hat)):
     prefactor = 2 * f_pbh * d_s / (m_pbh)
     first_term = rho_M31(x) * v_r(x, u_min, m_pbh, t_hat)**4 * np.exp(-(v_r(x, u_min, m_pbh, t_hat) / v_0_M31)**2) / (v_0_M31 **2 * np.sqrt(u_T - u_min**2))
-    second_term = rho_MW(x) * v_r(x, u_min, m_pbh, t_hat)**4 * np.exp(-(v_r(x, u_min, m_pbh, t_hat) / v_0_M31)**2) / (v_0_MW **2 * np.sqrt(u_T - u_min**2))
-    return prefactor * (first_term + second_term)
+    return prefactor * first_term
 
-def dgamma(m_pbh, t_hat):
-    return double_integral(dgamma_integrand, x_min, x_max, 0, u_T, args=(m_pbh, t_hat)) / t_hat**4
+def dgamma_MW(m_pbh, t_hat):
+    return double_integral(dgamma_integrand_MW, x_min, x_max, u_0, u_T - u_0, args=(m_pbh, t_hat), n_steps=1000) / t_hat**4
 
-t_hat_values = 10.**np.arange(-2, 1) / (365.25 * 24)
+def dgamma_M31(m_pbh, t_hat):
+    return double_integral(dgamma_integrand_M31, x_min, x_max, u_0, u_T - u_0, args=(m_pbh, t_hat), n_steps=1000) / t_hat**4
+
+
+hours_to_years = 1 / (365.25 * 24)
+t_hat_values = 10.**np.arange(-2, 1) * hours_to_years
 plt.figure()
 
-for m_pbh in 10.**np.arange(-11, -7.9, 1):
-    dgamma_values = []
+for m_pbh in 10.**np.arange(-8, -7.9, 1):
+    dgamma_MW_values = []
+    dgamma_M31_values = []
+    print(m_pbh)
     
     for t_hat in t_hat_values:
-        dgamma_values.append(dgamma(m_pbh, t_hat))
+        dgamma_MW_values.append(dgamma_MW(m_pbh, t_hat))
+        dgamma_M31_values.append(dgamma_M31(m_pbh, t_hat))
         
-    plt.plot(t_hat_values * 365.25 * 24, dgamma_values * (365.25 * 24)**2, label='1e{:.0f} $M_\odot$'.format(np.log10(m_pbh)))
+    plt.plot(np.array(t_hat_values) / hours_to_years, np.array(dgamma_MW_values) / hours_to_years**2, label='MW')
+    plt.plot(np.array(t_hat_values) / hours_to_years, np.array(dgamma_M31_values) / hours_to_years**2, label='M31')
+    plt.plot(np.array(t_hat_values) / hours_to_years, (np.array(dgamma_M31_values) + np.array(dgamma_MW_values)) / hours_to_years**2, label='Total')
 
 plt.xscale('log')
 plt.yscale('log')
 plt.legend()
 plt.xlabel('$\hat{t}$ [hr]')
 plt.ylabel('Event rate [hr]$^{-2}$')
+
