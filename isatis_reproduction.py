03e1c0e6de88e7a78cbeab763b8c87dec3d33df2
gortonm <matthew.gorton@nottingham.ac.uk>
2022-09-25 17:48:13 +0100

    Corrected input functions.

--- isatis_reproduction.py
+++ isatis_reproduction.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from reproduce_COMPTEL_constraints_v2 import load_data, read_col, read_blackhawk_spectra, string_scientific
+from J_factor_A22 import j_avg
 
 # Script to understand the meaning of the 'refined flux' values used in Isatis
 
@@ -47,18 +48,18 @@ def refined_energies(energies, n_refined):
         ener_refined.append( 10**(np.log10(E_min/10) + i*step) )
     return ener_refined
 
-def refined_flux(flux, ener_spec, energies, n_refined):
+def refined_flux(flux, ener_spec, n_refined):
     ener_refined = refined_energies(energies, n_refined)
     
-    nb_ener = len(energies)
+    nb_spec = len(ener_spec)
     
     flux_refined = []
     
     c = 0
     for i in range(n_refined):
-        while c < nb_ener and ener_spec[c] < ener_refined[i]:
+        while c < nb_spec-1 and ener_spec[c] < ener_refined[i]:   # modified: has c < nb_ener - 1 instead of c < nb_ener
             c += 1
-        if c > 0 and c < nb_ener and flux[c-1] !=0:
+        if c > 0 and c < nb_spec and flux[c-1] !=0:
             y = np.log10(flux[c-1]) + ((np.log10(flux[c]) - np.log10(flux[c-1])) / (np.log10(ener_spec[c]) - np.log10(ener_spec[c-1]))) * (np.log10(ener_refined[i]) - np.log10(ener_spec[c-1]))
             flux_refined.append(10**y)
         else: 
@@ -66,25 +67,20 @@ def refined_flux(flux, ener_spec, energies, n_refined):
             
     return flux_refined
 
-def refined_flux_2(flux, ener_spec, energies, n_refined):
-    ener_refined = refined_energies(energies, n_refined)
-    
-    nb_ener = len(energies)
-    
-    flux_refined = []
-    
-    c = 0
-    for i in range(n_refined):
-        while c < nb_ener and ener_spec[c] < ener_refined[i]:
-            c += 1
-        if c > 0 and c < nb_ener and flux[c-1] !=0:
-            y = np.log10(flux[c-1]) + ((np.log10(flux[c]) - np.log10(flux[c-1])) / (np.log10(energies[c]) - np.log10(energies[c-1]))) * (np.log10(ener_refined[i]) - np.log10(energies[c-1]))
-            flux_refined.append(10**y)
-        else: 
-            flux_refined.append(0)
-            
-    return flux_refined
 
+def galactic(spectrum):
+    n_spec = len(spectrum)
+    
+    # Calculate J-factor
+    b_max_Auffinger, l_max_Auffinger = np.radians(15), np.radians(30)
+    j_factor = 2 * j_avg(b_max_Auffinger, l_max_Auffinger)
+    
+    galactic = []
+    for i in range(n_spec):
+        val = j_factor[0] * spectrum[i] / (4*np.pi*m_pbh)
+        galactic.append(val)
+        
+    return galactic
 
 m_pbh = 1e15
 
@@ -106,15 +102,13 @@ energies = np.array([1.23204e-03, 4.47746e-03, 1.26645e-02])
 
 n_refined = 500
 
+flux_galactic = galactic(spectrum)
 ener_refined = refined_energies(energies, n_refined)
-flux_refined = refined_flux_2(flux, ener_spec, energies, n_refined)
-print(flux_refined)
-
-flux_refined = refined_flux(flux, ener_spec, energies, n_refined)
+flux_refined = refined_flux(flux_galactic, ener_spec, n_refined)
 print(flux_refined)
 
 plt.figure()
-plt.plot(energies, flux, 'x', label = 'Original')
+plt.plot(ener_spec, flux_galactic, 'x', label = 'Original')
 plt.plot(ener_refined, flux_refined, 'x', label='Refined')
 plt.title('$M_\mathrm{PBH}$ = ' + "{:.1f}e{:.0f}".format(coefficient, exponent) + 'g')
 plt.xlabel('Energy [GeV]')
