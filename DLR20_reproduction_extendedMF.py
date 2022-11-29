4feccc618b6ec011e8cb30a0e1b401c0e6048f8c
gortonm <matthew.gorton@nottingham.ac.uk>
2022-11-29 16:09:42 +0000

    Reproduce dn/dM dM as found in BH_spectrum.txt from BlackHawk.

--- DLR20_reproduction_extendedMF.py
+++ DLR20_reproduction_extendedMF.py
@@ -64,7 +64,7 @@ rho_odot = 0.4 * GeV_to_g
 r_odot = 8.5 * kpc_to_cm
 r_s = 20 * kpc_to_cm
 
-sigma = 1.
+sigma = 1.0
 
 # inferred rate of positron annihilation, {from observations of the 511 keV
 # signal (in s^{-1}).
@@ -141,6 +141,9 @@ def main():
         coefficient = m_pbh / 10**exponent
 
         file_path_data = file_path_data_base + "sigma={:.1f}/mu={:.1f}e{:.0f}g/".format(sigma,coefficient, exponent)
+        
+        if sigma < 0.5:
+            file_path_data = file_path_data_base + "sigma={:.2f}/mu={:.1f}e{:.0f}g/".format(sigma,coefficient, exponent)
 
         if primary_only:
             ep_energies_load, ep_spec_load = read_blackhawk_spectra(file_path_data + "instantaneous_primary_spectra.txt", col=7)
@@ -157,9 +160,13 @@ def main():
 #%%
 if __name__ == "__main__":
 
-    # load results from Fig. 1 of 1912.01014, for PBHs with zero spin.
-    file_path_extracted = "./Extracted_files/"
-    m_pbh_NFW_3500pc, f_pbh_NFW_3500pc = load_data("DLR20_Fig2_LN_sigma={:.1f}.csv".format(sigma))
+    if sigma < 0.5:
+        file_path_extracted = "./Extracted_files/"
+        m_pbh_NFW_3500pc, f_pbh_NFW_3500pc = load_data('DLR20_Fig2_a__0_newaxes_2.csv')
+    else:
+        file_path_extracted = "./Extracted_files/"
+        m_pbh_NFW_3500pc, f_pbh_NFW_3500pc = load_data("DLR20_Fig2_LN_sigma={:.1f}.csv".format(sigma))
+    
 
     # calculate constraint on fraction of dark matter in PBHs.
     f_pbh_values = []
@@ -168,40 +175,47 @@ if __name__ == "__main__":
     
     # plot the results from Fig. 1 of 1912.01014, for PBHs with zero spin.
     plt.figure(figsize=(7, 6))
-    plt.plot(m_pbh_values, f_pbh_values, 'x', linestyle='none', label='Reproduction', color='tab:blue')
-    plt.plot(m_pbh_NFW_3500pc, f_pbh_NFW_3500pc, label='Fig. 2 (DLR (2020))', color='tab:orange')
+    plt.plot(mu_pbh_values, f_pbh_values, 'x', linestyle='none', label='Reproduction', color='tab:blue')
+    plt.plot(m_pbh_NFW_3500pc, f_pbh_NFW_3500pc, label='Fig. 2 \n (DLR (2020))', color='tab:orange')
     plt.xscale('log')
     plt.yscale('log')
     plt.ylabel('$f_\mathrm{PBH}$')
     plt.xlabel('$\mu_\mathrm{PBH}$ [g]')
     plt.xlim(1e15, 1e19)
     plt.ylim(1e-4, 1)
-    plt.title("Log-normal ($\sigma={:.1f}$)".format(sigma))
+    if sigma < 0.5:
+        plt.title("Log-normal ($\sigma={:.2f}$)".format(sigma))
+    else:
+        plt.title("Log-normal ($\sigma={:.1f}$)".format(sigma))
+    plt.legend(fontsize='small')
     plt.tight_layout()
 
     
     # calculate ratio between reproduced results and those from Fig. 1 of
     # 1912.01014.
-    f_pbh_interp = np.interp(m_pbh_values, m_pbh_NFW_3500pc, f_pbh_NFW_3500pc)
+    f_pbh_interp = np.interp(mu_pbh_values, m_pbh_NFW_3500pc, f_pbh_NFW_3500pc)
     ratio = np.array(f_pbh_values / f_pbh_interp)
     print(ratio)
 
     # plot the fractional difference between the results
     plt.figure(figsize=(7, 6))
-    plt.plot(m_pbh_values, ratio-1, 'x', linestyle='none', color='tab:blue')
+    plt.plot(mu_pbh_values, ratio-1, 'x', linestyle='none', color='tab:blue')
     plt.xscale('log')
     #plt.yscale('log')
     plt.ylabel('$f_\mathrm{PBH, calculated}/f_\mathrm{PBH, extracted}$ - 1')
     plt.xlabel('$\mu_\mathrm{PBH}$ [g]')
     plt.xlim(1e15, 5e18)
-    plt.title("Log-normal ($\sigma={:.1f}$)".format(sigma))
+    if sigma < 0.5:
+        plt.title("Log-normal ($\sigma={:.2f}$)".format(sigma))
+    else:
+        plt.title("Log-normal ($\sigma={:.1f}$)".format(sigma))
     plt.tight_layout()
 
 
 #%%
 
-sigma = 1.
-m_pbh_full = np.logspace(9, 22, 1000)
+sigma = 0.5
+m_pbh_full = np.logspace(14.5, 15.5, 1000)
 
 def LN_MF_density(m, m_c, sigma, A=1):
     return A * np.exp(-np.log(m/m_c)**2 / (2*sigma**2)) / (np.sqrt(2*np.pi) * sigma * m**2)
@@ -215,7 +229,7 @@ def read_blackhawk_MF(fname):
     dndm = read_col(fname, first_row=4, col=1, convert=float)
     return np.array(m), np.array(dndm)
 
-mu_pbh = 3e18
+mu_pbh = 1e15
 print("\n mu_PBH = {:.2e} g".format(mu_pbh))
 print("\n exp(ln(mu_pbh) - sigma^2) = {:.2e} g".format(np.exp(np.log(mu_pbh) - sigma**2)))
 #print("\n exp(ln(mu_pbh) - 2sigma^2) = {:.2e} g".format(np.exp(np.log(mu_pbh) - 2*sigma**2)))
@@ -225,16 +239,74 @@ coefficient = mu_pbh / 10**exponent
 
 file_path_data = file_path_data_base + "sigma={:.1f}/mu={:.1f}e{:.0f}g/".format(sigma, coefficient, exponent)
 
-m_pbh, dndm = read_blackhawk_MF(file_path_data + "BH_spectrum.txt")
-print("{:.2e}".format(m_pbh[np.argmax(dndm)]))
+if sigma >= 0.5:
+    m_pbh, dndm = read_blackhawk_MF(file_path_data + "BH_spectrum.txt")
+    print("{:.2e}".format(m_pbh[np.argmax(dndm)]))
     
-plt.figure(figsize=(7, 6))
-plt.plot(m_pbh_full, LN_MF_density(m_pbh_full, mu_pbh, sigma), label='Mass density')
-plt.plot(m_pbh_full, LN_MF_number_density(m_pbh_full, mu_pbh, sigma), label='Number density')
-plt.plot(m_pbh, dndm, label='from BlackHawk')
+plt.figure(figsize=(8, 7))
+plt.plot(m_pbh, LN_MF_density(m_pbh, mu_pbh, sigma), label='Mass density LN')
+plt.plot(m_pbh, LN_MF_number_density(m_pbh, mu_pbh, sigma), label='Number density LN')
+if sigma >= 0.5:
+    plt.plot(m_pbh, dndm, label='from BlackHawk')
 plt.xlabel('$M_\mathrm{PBH}$ [g]')
+plt.ylabel('$\mathrm{d}n / \mathrm{d}M_\mathrm{PBH} ~ [\mathrm{cm}^{-3}]$')
 plt.yscale('log')
 plt.xscale('log')
+plt.ylim(1e-20 * max(dndm), 1e5 * max(dndm))
+plt.xlim(min(m_pbh), max(m_pbh))
 plt.legend(fontsize='small')
-plt.title("$\mu = {:.1e}~$g$, ~\sigma={:.1f}$".format(mu_pbh, sigma))
+plt.title("$\mu_\mathrm{PBH} " + "= {:.1e}~$g$, ~\sigma={:.1f}$".format(mu_pbh, sigma))
 plt.tight_layout()
+
+print(dndm / LN_MF_density(m_pbh, mu_pbh, sigma))
+print(dndm / LN_MF_number_density(m_pbh, mu_pbh, sigma))
+
+
+plt.figure(figsize=(8, 7))
+plt.plot(m_pbh, LN_MF_density(m_pbh, mu_pbh, sigma) / np.sum(LN_MF_density(m_pbh, mu_pbh, sigma)), label='Mass density LN')
+plt.plot(m_pbh, LN_MF_number_density(m_pbh, mu_pbh, sigma) / np.sum(LN_MF_number_density(m_pbh, mu_pbh, sigma)), label='Number density LN')
+plt.xlabel('$M_\mathrm{PBH}$ [g]')
+#plt.ylabel('$\mathrm{d}n / \mathrm{d}M_\mathrm{PBH} ~ [\mathrm{cm}^{-3}]$')
+plt.ylabel('$\mathrm{d}n / \mathrm{d}M_\mathrm{PBH}$ (normalised)')
+plt.yscale('log')
+plt.xscale('log')
+plt.ylim(1e-15, 1e-1)
+#plt.xlim(min(m_pbh), max(m_pbh))
+#plt.xlim(1e13, 5e20)
+plt.xlim(1e11, 1e18)
+plt.legend(fontsize='small')
+plt.title("$\mu_\mathrm{PBH} " + "= {:.1e}~$g$, ~\sigma={:.1f}$".format(mu_pbh, sigma))
+plt.tight_layout()
+
+
+
+#%% Reproduce the quantity in BH_spectrum.txt [i.e. (dn/dM)dM]
+
+sigma = 1.0
+mu_pbh = 1e15
+
+exponent = np.floor(np.log10(mu_pbh))
+coefficient = mu_pbh / 10**exponent
+
+file_path_data = file_path_data_base + "sigma={:.1f}/mu={:.1f}e{:.0f}g/".format(sigma, coefficient, exponent)
+
+m_pbh, dndm = read_blackhawk_MF(file_path_data + "BH_spectrum.txt")
+
+m_min, m_max = min(m_pbh), max(m_pbh)
+delta_log_m = 0.5 * (np.log10(m_max) - np.log10(m_min)) / (len(m_pbh) - 1)
+
+def create_spec_table(m_pbh, dndm):
+    spec_table = []
+    
+    for i in range(len(m_pbh)):
+        x = np.log10(m_pbh[i]) + delta_log_m
+        y = np.log10(m_pbh[i]) - delta_log_m
+        spec_table.append(dndm[i] * (10**x - 10**y))
+    
+    return spec_table
+
+dndm_LN_mass = LN_MF_density(m_pbh, mu_pbh, sigma, A=1)
+
+print(create_spec_table(m_pbh, dndm_LN_mass)[10])
+print(create_spec_table(m_pbh, dndm_LN_mass)[100])
+print(create_spec_table(m_pbh, dndm_LN_mass)[500])
