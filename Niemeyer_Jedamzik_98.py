ff1b9fd3635aae1d7b5924cd253ac4044ea76c04
gortonm <matthew.gorton@nottingham.ac.uk>
2023-01-30 12:56:13 +0000

    Plot the integrand and different approximations for Omega_PBH.

---
+++ Niemeyer_Jedamzik_98.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 30 12:01:35 2023
+
+@author: ppxmg2
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from scipy.special import erf
+
+# Check certain assumptions in the derivations in Niemeyer & Jedamzik (1998)
+# (astro-ph/9709072)
+
+# Specify the plot style
+mpl.rcParams.update({'font.size': 16,'font.family':'serif'})
+mpl.rcParams['xtick.major.size'] = 7
+mpl.rcParams['xtick.major.width'] = 1
+mpl.rcParams['xtick.minor.size'] = 3
+mpl.rcParams['xtick.minor.width'] = 1
+mpl.rcParams['ytick.major.size'] = 7
+mpl.rcParams['ytick.major.width'] = 1
+mpl.rcParams['ytick.minor.size'] = 3
+mpl.rcParams['ytick.minor.width'] = 1
+mpl.rcParams['xtick.direction'] = 'in'
+mpl.rcParams['ytick.direction'] = 'in'
+mpl.rcParams['lines.linewidth'] = 1.5
+mpl.rcParams['xtick.top'] = False
+mpl.rcParams['ytick.right'] = False
+mpl.rcParams['font.family'] = 'serif'
+mpl.rc('text', usetex=True)
+mpl.rcParams['legend.edgecolor'] = 'lightgrey'
+
+delta_c = 1/3
+gamma = 0.356
+k = 1
+
+#%%
+
+def delta_m(sigma):
+    return 0.5 * (delta_c + np.sqrt(4*gamma*sigma**2 + delta_c**2))
+    
+def delta_m_approx(sigma):
+    return delta_c + gamma*sigma**2 / delta_c
+
+def integrand(delta, sigma):
+    return np.power(delta-delta_c, gamma) * np.exp(-delta**2 / (2*sigma**2)) / (sigma * np.sqrt(2*np.pi))
+
+def omega_PBH_new(sigma):
+    return 0.5 * k * np.power(sigma, 2*gamma) * (erf(1/(np.sqrt(2)*sigma)) - erf(delta_c/(np.sqrt(2)*sigma)))
+    
+def omega_PBH_new_approx(sigma):
+    return (np.sqrt(2)/4) * k * np.power(sigma, 1 + 2*gamma) * ((np.exp(-delta_c**2 / (2*sigma**2))/delta_c) - (np.exp(- 1 / (2*sigma**2))))
+    
+def omega_PBH_new_approx_v2(sigma):
+    return k * np.power(sigma, 1 + 2*gamma) * np.exp(-delta_c**2 / (2*sigma**2))
+
+
+sigmas_integrand = np.logspace(-2, -1.0, 5)
+deltas = np.logspace(np.log10(delta_c), -1, 1000)
+for sigma in sigmas_integrand:
+    plt.figure()
+    plt.plot(deltas, integrand(deltas, sigma)/max(integrand(deltas, sigma)), label="${:.1e}$".format(sigma))
+    plt.gca().vlines(delta_m(sigma), ymin=0, ymax=1, color="k", linestyle="dashed", alpha=0.5)
+    plt.gca().vlines(delta_m_approx(sigma), ymin=0, ymax=1, color="k", linestyle="dotted", alpha=0.5)
+    plt.xlabel("$\delta$")
+    plt.ylabel("Integrand (normalised to peak value)")
+    plt.xscale("log")
+    #plt.yscale("log")
+    plt.legend(title="$\sigma$")
+    plt.tight_layout()
+
+sigmas = np.logspace(-2, -1, 1000)
+plt.figure()
+plt.plot(sigmas, omega_PBH_new(sigmas), label="Full expression")
+plt.plot(sigmas, omega_PBH_new_approx(sigmas), label="Large $x$ expansion of $\mathrm{erf}(x)$")
+plt.plot(sigmas, omega_PBH_new_approx_v2(sigmas), label="Expression from Eq. (7)")
+
+plt.xlabel("$\sigma$")
+plt.ylabel("$\hat{\Omega}_\mathrm{PBH, new} / k$")
+plt.xscale("log")
+plt.yscale("log")
+plt.legend()
+plt.tight_layout()
+
+# compare to 
