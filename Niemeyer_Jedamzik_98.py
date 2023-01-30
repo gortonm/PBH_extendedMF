afc6f75f476fa0205ee4ee6f40a6b2eb7d341ce0
gortonm <matthew.gorton@nottingham.ac.uk>
2023-01-30 17:53:22 +0000

    Add calculation of the 'new' Omega_PBH using the trapezium rule.

--- Niemeyer_Jedamzik_98.py
+++ Niemeyer_Jedamzik_98.py
@@ -37,6 +37,12 @@ delta_c = 1/3
 gamma = 0.356
 k = 1
 
+# choose range of sigma between 0.1 delta_c and 0.2 delta_c, corresponding
+# to the comment in the paper that this range of sigma generates a
+# cosmologically interesting number of PBHs, for Gaussian statistics.
+sigma_min = 0.1*delta_c
+sigma_max = 0.2*delta_c
+
 #%%
 
 def delta_m(sigma):
@@ -46,7 +52,7 @@ def delta_m_approx(sigma):
     return delta_c + gamma*sigma**2 / delta_c
 
 def integrand(delta, sigma):
-    return np.power(delta-delta_c, gamma) * np.exp(-delta**2 / (2*sigma**2)) / (sigma * np.sqrt(2*np.pi))
+    return k * np.power(delta-delta_c, gamma) * np.exp(-delta**2 / (2*sigma**2)) / (sigma * np.sqrt(2*np.pi))
 
 def omega_PBH_new(sigma):
     return 0.5 * k * np.power(sigma, 2*gamma) * (erf(1/(np.sqrt(2)*sigma)) - erf(delta_c/(np.sqrt(2)*sigma)))
@@ -57,12 +63,17 @@ def omega_PBH_new_approx(sigma):
 def omega_PBH_new_approx_v2(sigma):
     return k * np.power(sigma, 1 + 2*gamma) * np.exp(-delta_c**2 / (2*sigma**2))
 
+def omega_PBH_new_numeric(sigma):
+    deltas_integral = np.logspace(np.log10(delta_c), 0, 10000)
+    integrand_values = [integrand(delta, sigma) for delta in deltas_integral]
+    return np.trapz(integrand_values, deltas_integral)
 
-sigmas_integrand = np.logspace(-2, -1.0, 5)
-deltas = np.logspace(np.log10(delta_c), -1, 1000)
+sigmas_integrand = np.logspace(np.log10(sigma_min), np.log10(sigma_max), 5)
+deltas = np.logspace(np.log10(delta_c), 0, 1000)
+
 for sigma in sigmas_integrand:
     plt.figure()
-    plt.plot(deltas, integrand(deltas, sigma)/max(integrand(deltas, sigma)), label="${:.1e}$".format(sigma))
+    plt.plot(deltas, integrand(deltas, sigma)/max(integrand(deltas, sigma)), label="${:.3f}$".format(sigma))
     plt.gca().vlines(delta_m(sigma), ymin=0, ymax=1, color="k", linestyle="dashed", alpha=0.5)
     plt.gca().vlines(delta_m_approx(sigma), ymin=0, ymax=1, color="k", linestyle="dotted", alpha=0.5)
     plt.xlabel("$\delta$")
@@ -72,17 +83,15 @@ for sigma in sigmas_integrand:
     plt.legend(title="$\sigma$")
     plt.tight_layout()
 
-sigmas = np.logspace(-2, -1, 1000)
+sigmas = np.logspace(np.log10(sigma_min), np.log10(sigma_max), 1000)
+omega_PBH_trapz = [omega_PBH_new_numeric(sigma) for sigma in sigmas]
+
 plt.figure()
-plt.plot(sigmas, omega_PBH_new(sigmas), label="Full expression")
-plt.plot(sigmas, omega_PBH_new_approx(sigmas), label="Large $x$ expansion of $\mathrm{erf}(x)$")
-plt.plot(sigmas, omega_PBH_new_approx_v2(sigmas), label="Expression from Eq. (7)")
+plt.plot(sigmas, omega_PBH_trapz, label="Trapezium rule")
+plt.plot(sigmas, omega_PBH_new_approx_v2(sigmas), label="Approximate expression from Eq. (7)")
 
 plt.xlabel("$\sigma$")
 plt.ylabel("$\hat{\Omega}_\mathrm{PBH, new} / k$")
-plt.xscale("log")
 plt.yscale("log")
 plt.legend()
 plt.tight_layout()
-
-# compare to 
