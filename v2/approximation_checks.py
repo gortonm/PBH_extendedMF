ea28df306f211a20a5ec7123c080265c73007c50
gortonm <matthew.gorton@nottingham.ac.uk>
2023-03-19 17:23:17 +0000

    Initial implementation of accounting for the effect of evaporation on the PBH mass function, using the formalism from 1604.05349.

--- v2/approximation_checks.py
+++ v2/approximation_checks.py
@@ -120,3 +120,144 @@ ax.legend(fontsize="small")
 ax.set_xlim(2e13, 1e19)
 ax.set_ylim(1e10, 1e23)
 
+#%% Account for the behaviour of the mass function at small PBH masses,
+# following the approximate results from 1604.05349.
+
+M_star = 5e14   # formation mass of PBH evaporating at present, in grams
+m_q = 2e14    # formation mass of PBH with a temperature above the QCD temperature (see Eq. 2.4 of 1604.05349)
+alpha_evap = 4   # related to emitted number of particle degrees of freedom (see Eqs. 2.7-2.8 of 1604.05349)
+
+q = 0.4   # ratio of m_q to m_star
+M_c = np.power(1 + q**3/alpha_evap, 1/3) * M_star   # characteristic mass (see Eq. (2.18) of 1604.05349)
+
+def mf_evap_effects(m, mf, m_c, params):
+    """
+    Approximate present-day PBH mass function, obtained by considering the
+    effect of Hawking evaporation on the mass function evaluated at the 
+    formation mass, following the approach from 1604.05349.
+
+    Parameters
+    ----------
+    m : Array-like
+        PBH masses (at present).
+    mf : Function
+        PBH mass function (at formation).
+    m_c : Float
+        Characteristic PBH mass (at formation).
+    params : Array-like
+        Parameters of the PBH mass function.
+
+    Returns
+    -------
+    mf_values : Array-like
+        Values of the present-day PBH mass function, evaluated at the formation
+        mass.
+
+    """
+    mf_values = []
+    
+    for i in range(len(m)):
+        if m[i] > M_star:
+            mf_values.append(mf(m[i], m_c, *params))
+        elif m_q < m[i] <= M_star:
+            mf_values.append(np.power(m[i]/M_star, 2) * mf(M_star, m_c, *params))
+        elif m[i] < m_q:
+            mf_values.append(np.power(m[i]/M_star, 2) * mf(M_star, m_c, *params) / alpha_evap)
+          
+    return mf_values
+
+
+def m_0(M):
+    """
+    Calculate present value of the PBH mass from the PBH mass at formation,
+    using Eq. (2.18) of 1604.05349.
+
+    Parameters
+    ----------
+    M : Array-like
+        PBH masses (at formation).
+
+    Returns
+    -------
+    m0_values : Array-like (at present)
+        PBH masses (at present).
+
+    """
+    m0_values = []
+    
+    for i in range(len(M)):
+        if M[i] > M_c:
+            m0_values.append(np.power(M[i]**3 - M_star**3 + (1+(1/alpha_evap))*(q*M_star)**3, 1/3))
+        elif M[i] < M_star:
+            m0_values.append(0)
+        else:
+            m0_values.append(np.power(alpha_evap * (M[i]**3 - M_star**3), 1/3))
+            
+    return np.array(m0_values)
+             
+
+if "__main__" == __name__:
+    
+    # Test: present PBH mass against formation mass.
+    # Reproduce Fig. 2 (left-hand panel) of 1604.05349.
+    M_values = np.logspace(11, 21, 1000)    # PBH masses, at formation
+    
+    fig, ax = plt.subplots(figsize=(5, 4))
+    ax.plot(M_values, m_0(M_values))
+    ax.set_xlabel("$M~[\mathrm{g}]$")
+    ax.set_ylabel("$m~[\mathrm{g}]$")
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_xlim(1e14, 1e17)
+    ax.set_ylim(1e12, 1e17)
+    ax.vlines(M_star, color="grey", linestyle="dotted", ymin=1e12, ymax=1e17)
+    fig.tight_layout()
+    
+    
+    # Plot effect of evaporation on PBH mass function.
+    
+    # Load mass function parameters.
+    [Deltas, sigmas_LN, ln_mc_SLN, mp_SLN, sigmas_SLN, alphas_SLN, mp_CC3, alphas_CC3, betas] = np.genfromtxt("MF_params.txt", delimiter="\t\t ", skip_header=1, unpack=True)
+
+    mc_values = np.logspace(14, 19, 5)
+    m_present = m_0(M_values)
+    
+    for m_c in mc_values:
+    
+        for i in range(len(Deltas[:-1])):
+                        
+            params_LN = [sigmas_LN[i]]
+            params_SLN = [sigmas_SLN[i], alphas_SLN[i]]
+            params_CC3 = [alphas_CC3[i], betas[i]]
+            
+            LN_formation = LN(M_values, m_c, sigmas_LN[i])
+            SLN_formation = SLN(M_values, m_c, sigmas_SLN[i], alphas_SLN[i])
+            CC3_formation = CC3(M_values, m_c, alphas_CC3[i], betas[i])
+    
+            LN_present = mf_evap_effects(m_present, mf=LN, m_c=m_c, params=params_LN)
+            SLN_present = mf_evap_effects(m_present, mf=SLN, m_c=m_c, params=params_SLN)
+            CC3_present = mf_evap_effects(m_present, mf=CC3, m_c=m_c, params=params_CC3)
+            
+            fig, ax = plt.subplots(figsize=(7, 5))
+            ax1 = ax.twinx()
+            
+            ax.plot(m_present, LN_present / max(LN_formation), color="r", label="LN")
+            ax.plot(M_values, LN_formation / max(LN_formation), linestyle="dotted", color="r")
+
+            ax.plot(m_present, SLN_present / max(SLN_formation), color="b", label="SLN")
+            ax.plot(M_values, SLN_formation / max(SLN_formation), linestyle="dotted", color="b")
+
+            ax.plot(m_present, CC3_present / max(CC3_formation), color="g", label="CC3")
+            ax.plot(M_values, CC3_formation / max(CC3_formation), linestyle="dotted", color="g")
+            
+            ax.set_xlim(m_c / 100, m_c * 100)
+            ax.set_ylim(1e-6, 2)
+
+            ax.set_xlabel("$m~[\mathrm{g}]$")
+            ax.set_ylabel("$\psi / \psi_{f, \mathrm{max}}$")
+            ax.legend(title=r"$M_c={:.1e}".format(m_c) + "~[\mathrm{g}]," + "~\Delta={:.1f}$".format(Deltas[i]))
+            ax1.set_xlabel("$M~[\mathrm{g}]$")
+            ax.set_xscale("log")
+            ax.set_yscale("log")
+            fig.tight_layout()
+          
