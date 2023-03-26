24821d943f0d48dbf9eb213d564c69a4f360ec02
gortonm <matthew.gorton@nottingham.ac.uk>
2023-03-26 12:01:19 +0100

    Add loop so that the program automatically runs for both PYTHIA and Hazma. Remove print(command).

--- v2/isatis_runner_mono.py
+++ v2/isatis_runner_mono.py
@@ -33,7 +33,7 @@ E_number = 1000
 BH_number = 1
 
 # Range of PBH masses
-m_pbh_values = np.logspace(11, 21, 1000)
+m_pbh_values = np.logspace(11, 21, 100)
 
 # Path to BlackHawk and Isatis
 BlackHawk_path = os.path.expanduser('~') + "/Downloads/version_finale/"
@@ -56,7 +56,7 @@ params_Isatis[8][1] = "{:.0f}".format(r_c_halo)
 params_Isatis[9][1] = "{:.0f}".format(gamma_halo)     
 params_Isatis[14][1] = "1"   
 
-E_min, E_max = 1e-6, 105.874        
+E_min_full_range, E_max_full_range = 1e-6, 105.874        
                 
 E_c = 5     # cutoff energy above which to use PYTHIA hadronisation tables
 energies = np.logspace(np.log10(E_min), np.log10(E_max), E_number)
@@ -69,10 +69,6 @@ E_PYTHIA = energies[energies > E_c]
 E_number_PYTHIA = len(E_PYTHIA)
 E_min_PYTHIA = min(E_PYTHIA)
 
-# Boolean controls whether to use PYTHIA or Hazma tables
-PYTHIA = False
-Hazma = not PYTHIA
-
 fname_base = "GC_mono"
 os.chdir(os.path.expanduser('~') + "/Asteroid_mass_gap/v2")
 # Save Isatis parameters file.
@@ -80,55 +76,61 @@ params_Isatis[0][1] = fname_base
 filename_params_Isatis = "params_%s.txt" % fname_base
 np.savetxt(Isatis_path + filename_params_Isatis, params_Isatis, fmt="%s", delimiter = " = ")
 
-if PYTHIA:
-    case = 2
-    spectrum_folder = "_PYTHIA"
-    E_number = E_number_PYTHIA
-    E_min = E_min_PYTHIA
-elif Hazma:
-    case = 3
-    spectrum_folder = "_Hazma"
-    E_number = E_number_Hazma
-    E_max = E_max_Hazma
-                                                                                    
-# Create runs file
-runs_filename = "runs_%s.txt" % fname_base
-runs_file_content = []
-runs_file_content.append("nb_runs = {:.0f}".format(len(m_pbh_values)))
-runs_file_content.append("")
- 
-for j, m in enumerate(m_pbh_values):
-    
-    destination_folder = fname_base + "_{:.0f}".format(j) + spectrum_folder
-        
-    filename_BlackHawk = "/BH_launcher/" + destination_folder + ".txt"
-    # Add run name to runs file
-    runs_file_content.append(destination_folder)
-                        
-    params_BlackHawk[0][1] = destination_folder
-    params_BlackHawk[4][1] = "{:.0f}".format(BH_number)
-    params_BlackHawk[5][1] = "{:.5e}".format(m)
-    params_BlackHawk[6][1] = "{:.5e}".format(m)
-    params_BlackHawk[15][1] = "1"
-    params_BlackHawk[20][1] = "{:.5e}".format(m)
-    params_BlackHawk[34][1] = "{:.0f}".format(E_number)
-    params_BlackHawk[35][1] = "{:.5e}".format(E_min)
-    params_BlackHawk[36][1] = "{:.5e}".format(E_max)
-    params_BlackHawk[-1][1] = "{:.0f}".format(case)
-                                        
-    # Save BlackHawk parameters file.
-    np.savetxt(Isatis_path + filename_BlackHawk, params_BlackHawk, fmt="%s", delimiter = " = ")
-    
-    # Run BlackHawk
-    os.chdir(BlackHawk_path)
-    command = "./BlackHawk_inst.x " + "scripts/Isatis/BH_launcher/" + destination_folder + ".txt<input.txt"
-    print(command)
-    os.system(command) 
-   
-# Save runs file
-np.savetxt(BlackHawk_path + "scripts/Isatis/BH_launcher/%s" % runs_filename, runs_file_content, fmt="%s")
+# Boolean controls whether to use PYTHIA or Hazma tables
+for PYTHIA in [True, False]:
 
-#%% Create Isatis file and run.
+    Hazma = not PYTHIA
+    
+    if PYTHIA:
+        case = 2
+        spectrum_folder = "_PYTHIA"
+        E_number = E_number_PYTHIA
+        E_min = E_min_PYTHIA
+        E_max = E_max_full_range
+    elif Hazma:
+        case = 3
+        spectrum_folder = "_Hazma"
+        E_number = E_number_Hazma
+        E_min = E_min_full_range
+        E_max = E_max_Hazma
+                                                                                        
+    # Create runs file
+    runs_filename = "runs_%s.txt" % fname_base
+    runs_file_content = []
+    runs_file_content.append("nb_runs = {:.0f}".format(len(m_pbh_values)))
+    runs_file_content.append("")
+     
+    for j, m in enumerate(m_pbh_values):
+        
+        destination_folder = fname_base + "_{:.0f}".format(j) + spectrum_folder
+            
+        filename_BlackHawk = "/BH_launcher/" + destination_folder + ".txt"
+        # Add run name to runs file
+        runs_file_content.append(destination_folder)
+                            
+        params_BlackHawk[0][1] = destination_folder
+        params_BlackHawk[4][1] = "{:.0f}".format(BH_number)
+        params_BlackHawk[5][1] = "{:.5e}".format(m)
+        params_BlackHawk[6][1] = "{:.5e}".format(m)
+        params_BlackHawk[15][1] = "1"
+        params_BlackHawk[20][1] = "{:.5e}".format(m)
+        params_BlackHawk[34][1] = "{:.0f}".format(E_number)
+        params_BlackHawk[35][1] = "{:.5e}".format(E_min)
+        params_BlackHawk[36][1] = "{:.5e}".format(E_max)
+        params_BlackHawk[-1][1] = "{:.0f}".format(case)
+                                            
+        # Save BlackHawk parameters file.
+        np.savetxt(Isatis_path + filename_BlackHawk, params_BlackHawk, fmt="%s", delimiter = " = ")
+        
+        # Run BlackHawk
+        os.chdir(BlackHawk_path)
+        command = "./BlackHawk_inst.x " + "scripts/Isatis/BH_launcher/" + destination_folder + ".txt<input.txt"
+        os.system(command) 
+       
+    # Save runs file
+    np.savetxt(BlackHawk_path + "scripts/Isatis/BH_launcher/%s" % runs_filename, runs_file_content, fmt="%s")
+
+#%% Create combined secondary spectrum file and run Isatis.
 
 if "__main__" == __name__:
 
@@ -155,15 +157,15 @@ if "__main__" == __name__:
             energies_truncated_PYTHIA = energies_tot_PYTHIA[energies_tot_PYTHIA>=E_c]
             spectrum_truncated_PYTHIA = spectrum_tot_PYTHIA[energies_tot_PYTHIA>=E_c]
 
-            print(energies_truncated_Hazma)            
-            print(energies_truncated_PYTHIA)           
+            #print(energies_truncated_Hazma)            
+            #print(energies_truncated_PYTHIA)           
             
             # Check energies do not overlap
-            print(min(energies_truncated_PYTHIA))
-            print(E_min_PYTHIA)
+            #print(min(energies_truncated_PYTHIA))
+            #print(E_min_PYTHIA)
         
-            print(max(energies_truncated_Hazma))
-            print(E_max_Hazma)
+            #print(max(energies_truncated_Hazma))
+            #print(E_max_Hazma)
             
             # Concatenate arrays
             energies_tot = np.concatenate((energies_truncated_Hazma, energies_truncated_PYTHIA))
@@ -176,7 +178,7 @@ if "__main__" == __name__:
         command = "./Isatis.x %s ./BH_launcher/%s" % (filename_params_Isatis, runs_filename)
         os.system(command)
 
-#%% Plot monochromatic MF constraints.
+#%%
 
 from preliminaries import load_results_Isatis
 import matplotlib.pyplot as plt
@@ -194,7 +196,7 @@ if "__main__" == __name__:
         fig, ax = plt.subplots(figsize=(8, 8))
         
         for i in range(len(constraints_names)):
-            ax.plot(m_pbh_values, f_PBH_Isatis[i], label=constraints_names[i], color=colors_evap[i])
+            ax.plot(m_pbh_values, f_PBH_Isatis[i], label=constraints_names[i], color=colors_evap[i], marker="x", linestyle="None")
         
         ax.set_xlim(1e14, 1e18)
         ax.set_ylim(10**(-10), 1)
