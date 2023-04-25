4a54c497ab17d0784687e7783967f80c8b559244
gortonm <matthew.gorton@nottingham.ac.uk>
2023-04-25 12:13:51 +0100

    Add plots of the total (versus primary) positron/electron spectrum emitted by PBHs, and calculations of the integral of the spectrum over particle energies.

--- v2/approximation_checks.py
+++ v2/approximation_checks.py
@@ -9,7 +9,7 @@ Created on Sun Mar 19 11:55:35 2023
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from preliminaries import LN, SLN, CC3, load_results_Isatis
+from preliminaries import LN, SLN, CC3
 from isatis_reproduction import read_blackhawk_spectra
 
 # Produce plots of the Subaru-HSC microlensing constraints on PBHs, for
@@ -37,7 +37,7 @@ mpl.rcParams['legend.edgecolor'] = 'lightgrey'
 plt.style.use('tableau-colorblind10')
 filepath = './Extracted_files/'
 
-#%% Understand at which mass range secondary emission of photons from PBHs becomes significant.
+#%% Understand at which mass range secondary emission of photons and electrons/positrons from PBHs becomes significant.
 
 m_pbh_values = np.logspace(11, 21, 1000)
 
@@ -47,10 +47,10 @@ file_path_BlackHawk_data = "./../../Downloads/version_finale/results/"
 # Plot the primary and total photon spectrum at different BH masses.
 
 # Choose indices to plot
-indices = [201, 301, 401, 501, 601]
+indices = [401, 501, 601, 701]
 colors = ['#006BA4', '#FF800E', '#ABABAB', '#595959', '#5F9ED1', '#C85200']
 
-fig, ax = plt.subplots(figsize=(8, 6.5))
+fig, ax = plt.subplots(figsize=(6.5, 5))
 
 for j, i in enumerate(indices):
     m_pbh = m_pbh_values[i]
@@ -66,9 +66,35 @@ ax.set_ylabel(r"$\tilde{Q}_\gamma(E)~[\mathrm{GeV^{-1} \cdot \mathrm{cm}^{-2} \c
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.set_xlim(1e-5, 5)
-ax.set_ylim(1e10, 1e30)
+ax.set_ylim(1e10, 1e25)
 
 
+# Plot the primary and total electron/positron spectrum at different BH masses.
+
+# Choose indices to plot
+indices = [301, 371, 401, 501]
+colors = ['#006BA4', '#FF800E', '#ABABAB', '#595959', '#5F9ED1', '#C85200']
+
+fig, ax = plt.subplots(figsize=(6.5, 5))
+
+for j, i in enumerate(indices):
+    m_pbh = m_pbh_values[i]
+    energies_primary, spectrum_primary = read_blackhawk_spectra(file_path_BlackHawk_data + "GC_mono_{:.0f}/".format(i) + "instantaneous_primary_spectra.txt", col=7)
+    energies_tot, spectrum_tot = read_blackhawk_spectra(file_path_BlackHawk_data + "GC_mono_{:.0f}/".format(i) + "instantaneous_secondary_spectra.txt", col=2)
+    
+    ax.plot(energies_primary, spectrum_primary, linestyle="dotted", color=colors[j])
+    ax.plot(energies_tot, spectrum_tot, color=colors[j], label="{:.0e}".format(m_pbh))
+    
+ax.legend(title="$m~[\mathrm{g}]$", fontsize="small")
+ax.set_xlabel("$E~[\mathrm{GeV}]$")
+ax.set_ylabel(r"$\tilde{Q}_e(E)~[\mathrm{GeV^{-1} \cdot \mathrm{cm}^{-2} \cdot \mathrm{s}^{-1} \cdot \mathrm{sr}^{-1}}]$")
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_xlim(5.11e-4, 10)
+ax.set_ylim(1e16, 1e24)
+
+
+#%%
 # Plot the integral of primary and total photon spectrum over energy.
 integral_primary = []
 integral_secondary = []
@@ -82,12 +108,12 @@ for i in range(len(m_pbh_values)):
 
 fit_m_square = integral_primary[500] * np.power(m_pbh_values/m_pbh_values[500], -1)
 
-fig, ax = plt.subplots(figsize=(8, 6.5))
+fig, ax = plt.subplots(figsize=(6.5, 5))
 ax.plot(m_pbh_values, integral_secondary, color=colors[0], label="Total")
 ax.plot(m_pbh_values, integral_primary, linestyle="dashed", color=colors[0], label="Primary emission only")
 ax.plot(m_pbh_values, fit_m_square, color=colors[1], linestyle="dotted", label="$m^{-1}$ fit")
 ax.set_xlabel("$m~[\mathrm{g}]$")
-ax.set_ylabel("$\mathrm{d} N_\gamma/\mathrm{d}t~[\mathrm{cm}^{-2} \cdot \mathrm{s}^{-1} \cdot \mathrm{sr}^{-1}]$")
+ax.set_ylabel("$\mathrm{d} N_\gamma/\mathrm{d}t~[\mathrm{s}^{-1}]$")
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.legend(fontsize="small")
@@ -108,17 +134,69 @@ for i in range(len(m_pbh_values)):
 
 fit_m_square = integral_primary[500] * np.power(m_pbh_values/m_pbh_values[500], -2)
 
-fig, ax = plt.subplots(figsize=(8, 6.5))
+fig, ax = plt.subplots(figsize=(6.5, 5))
 ax.plot(m_pbh_values, integral_secondary, color=colors[0], label="Total")
 ax.plot(m_pbh_values, integral_primary, linestyle="dashed", color=colors[0], label="Primary emission only")
 ax.plot(m_pbh_values, fit_m_square, color=colors[1], linestyle="dotted", label="$m^{-2}$ fit")
 ax.set_xlabel("$m~[\mathrm{g}]$")
-ax.set_ylabel(r"$\int \tilde{Q}_\gamma(E) E \mathrm{d}E~[\mathrm{GeV} \cdot \mathrm{cm}^{-2} \cdot \mathrm{s}^{-1} \cdot \mathrm{sr}^{-1}]$")
+ax.set_ylabel(r"$\int \tilde{Q}_\gamma(E) E \mathrm{d}E~[\mathrm{GeV} \cdot \mathrm{s}^{-1}]$")
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.legend(fontsize="small")
 ax.set_xlim(2e13, 1e19)
 ax.set_ylim(1e10, 1e23)
+
+#%%
+# Plot the integral of primary and total electron spectrum over energy.
+integral_primary = []
+integral_secondary = []
+
+for i in range(len(m_pbh_values)):
+    energies_primary, spectrum_primary = read_blackhawk_spectra(file_path_BlackHawk_data + "GC_mono_{:.0f}/".format(i+1) + "instantaneous_primary_spectra.txt", col=7)
+    energies_tot, spectrum_tot = read_blackhawk_spectra(file_path_BlackHawk_data + "GC_mono_{:.0f}/".format(i+1) + "instantaneous_secondary_spectra.txt", col=2)
+            
+    integral_primary.append(np.trapz(spectrum_primary, energies_primary))
+    integral_secondary.append(np.trapz(spectrum_tot, energies_tot))
+
+fit_m_square = integral_primary[500] * np.power(m_pbh_values/m_pbh_values[500], -1)
+
+fig, ax = plt.subplots(figsize=(6.5, 5))
+ax.plot(m_pbh_values, integral_secondary, color=colors[0], label="Total")
+ax.plot(m_pbh_values, integral_primary, linestyle="dashed", color=colors[0], label="Primary emission only")
+ax.plot(m_pbh_values, fit_m_square, color=colors[1], linestyle="dotted", label="$m^{-1}$ fit")
+ax.set_xlabel("$m~[\mathrm{g}]$")
+ax.set_ylabel("$\mathrm{d} N_e/\mathrm{d}t~[\mathrm{s}^{-1}]$")
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.legend(fontsize="small")
+ax.set_xlim(1e14, 1e17)
+ax.set_ylim(1e18, 1.5e22)
+
+
+# Plot the integral of energy * primary and total electron spectrum over energy.
+integral_primary = []
+integral_secondary = []
+
+for i in range(len(m_pbh_values)):
+    energies_primary, spectrum_primary = read_blackhawk_spectra(file_path_BlackHawk_data + "GC_mono_{:.0f}/".format(i+1) + "instantaneous_primary_spectra.txt", col=7)
+    energies_tot, spectrum_tot = read_blackhawk_spectra(file_path_BlackHawk_data + "GC_mono_{:.0f}/".format(i+1) + "instantaneous_secondary_spectra.txt", col=2)
+            
+    integral_primary.append(np.trapz(spectrum_primary*energies_primary, energies_primary))
+    integral_secondary.append(np.trapz(spectrum_tot*energies_tot, energies_tot))
+
+fit_m_square = integral_primary[500] * np.power(m_pbh_values/m_pbh_values[500], -2)
+
+fig, ax = plt.subplots(figsize=(6.5, 5))
+ax.plot(m_pbh_values, integral_secondary, color=colors[0], label="Total")
+ax.plot(m_pbh_values, integral_primary, linestyle="dashed", color=colors[0], label="Primary emission only")
+ax.plot(m_pbh_values, fit_m_square, color=colors[1], linestyle="dotted", label="$m^{-2}$ fit")
+ax.set_xlabel("$m~[\mathrm{g}]$")
+ax.set_ylabel(r"$\int \tilde{Q}_e(E) E \mathrm{d}E~[\mathrm{GeV} \cdot \mathrm{s}^{-1}]$")
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.legend(fontsize="small")
+ax.set_xlim(1e14, 1e18)
+ax.set_ylim(1e13, 1e22)
 
 #%% Account for the behaviour of the mass function at small PBH masses,
 # following the approximate results from 1604.05349.
