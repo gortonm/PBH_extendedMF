Corrected calculation of integral over g(M) by including the required prefactor. Add plots comparing results.

v2/Carr_Bellomo_comparison.py
@@ -35,7 +35,7 @@ t_Pl = 5.391e-44    # Planck time, in seconds
 t_0 = 13.8e9 * 365.25 * 86400    # Age of Universe, in seconds
 m_star = 7.473420349255e+14    # Formation mass of a PBH with a lifetimt equal to the age of the Universe, in grams.
 
-"""
+
 #%% Constraints from COMPTEL, INTEGRAL, EGRET and Fermi-LAT. Approximate results obtained by using f_max as the constraint from each instrument, rather than the minimum over each energy bin.
 # Obtained using the method from Carr et al. (2017) [1705.05567].
 
@@ -43,12 +43,9 @@ if "__main__" == __name__:
     
     # Load mass function parameters.
     [Deltas, sigmas_LN, ln_mc_SLN, mp_SLN, sigmas_SLN, alphas_SLN, mp_CC3, alphas_CC3, betas] = np.genfromtxt("MF_params.txt", delimiter="\t\t ", skip_header=1, unpack=True)
-    
-    # Boolean determines whether to use evolved mass function.
-    evolved = True 
-    
+        
     # Boolean determines whether to use the full mass range where f_max is known or only the range where |f_max| < 1
-    all_fmax = False
+    all_fmax = True
         
     m_delta_values_loaded = np.logspace(11, 21, 1000)
     constraints_names, f_max_Isatis = load_results_Isatis(modified=True)
@@ -63,45 +60,44 @@ if "__main__" == __name__:
     else:
         data_folder = "./Data-tests/Carr_Bellomo_comparison/truncated_m_range"
         
-    # Power-law exponent to use
+    # Power-law exponent to uses
     exponent_PL_lower = 2.0
     m_delta_extrapolated = np.logspace(11, 13, 21)
     data_folder += "/PL_exp_{:.0f}/".format(exponent_PL_lower)
 
-    for j in range(len(Deltas)):
-        params_LN = [sigmas_LN[j]]
-        params_SLN = [sigmas_SLN[j], alphas_SLN[j]]
-        params_CC3 = [alphas_CC3[j], betas[j]]
+    sigma = 1.84859
+    evolved = False
         
-        for i in range(len(constraints_names)):
-            
-            # Only include PBH masses and values of f_max where |f_max| < 1
-            f_max_truncated = []
-            m_delta_loaded_truncated = []
-    
+    for i in range(len(constraints_names)):
+        f_max_allpositive = []
+        m_delta_loaded_allpositive = []
+
+        if all_fmax:
             for k, f_max in enumerate(f_max_Isatis[i]):
-                if abs(f_max) < 1:
-                    f_max_truncated.append(f_max)
-                    m_delta_loaded_truncated.append(m_delta_values_loaded[k])
-            
-            # Extrapolate f_max at masses below 1e13g using a power-law
-            f_max_loaded_truncated = np.array(f_max_truncated)[m_delta_values_loaded > 1e13]
-            f_max_extrapolated = f_max_loaded_truncated[0] * np.power(m_delta_extrapolated / 1e13, exponent_PL_lower)
-            f_max_i = np.concatenate((f_max_extrapolated, f_max_loaded_truncated))
-            m_delta_values = np.concatenate((m_delta_extrapolated, m_delta_values_loaded[m_delta_values_loaded > 1e13]))
-                        
-            f_PBH_i_LN = constraint_Carr(mc_values, m_delta_values, f_max_i, LN, params_LN, evolved, t)
-            f_PBH_i_SLN = constraint_Carr(mc_values, m_delta_values, f_max_i, SLN, params_SLN, evolved, t)
-            f_PBH_i_CC3 = constraint_Carr(mc_values, m_delta_values, f_max_i, CC3, params_CC3, evolved, t)
-      
-            data_filename_LN = data_folder + "/LN_GC_%s" % constraints_names_short[i] + "_Carr_Delta={:.1f}_approx.txt".format(Deltas[j])
-            data_filename_SLN = data_folder + "/SLN_GC_%s" % constraints_names_short[i]  + "_Carr_Delta={:.1f}_approx.txt".format(Deltas[j])
-            data_filename_CC3 = data_folder + "/CC3_GC_%s" % constraints_names_short[i]  + "_Carr_Delta={:.1f}_approx.txt".format(Deltas[j])
-    
-            np.savetxt(data_filename_LN, [mc_values, f_PBH_i_LN], delimiter="\t")
-            np.savetxt(data_filename_SLN, [mc_values, f_PBH_i_SLN], delimiter="\t")
-            np.savetxt(data_filename_CC3, [mc_values, f_PBH_i_CC3], delimiter="\t")
-"""
+                if f_max > 0:
+                    f_max_allpositive.append(f_max)
+                    m_delta_loaded_allpositive.append(m_delta_values_loaded[k])
+        else:
+            for k, f_max in enumerate(f_max_Isatis[i]):
+                if 0 < f_max <= 1:
+                    f_max_allpositive.append(f_max)
+                    m_delta_loaded_allpositive.append(m_delta_values_loaded[k])
+                    
+        m_max = max(m_delta_loaded_allpositive)
+        print("m_max = {:.8e} g".format(m_max))
+        
+        # Extrapolate f_max at masses below 1e13g using a power-law
+        f_max_loaded_truncated = np.array(f_max_allpositive)[np.array(m_delta_loaded_allpositive) > 1e13]
+        m_delta_loaded_truncated = np.array(m_delta_loaded_allpositive)[np.array(m_delta_loaded_allpositive) > 1e13]
+        f_max_extrapolated = f_max_loaded_truncated[0] * np.power(m_delta_extrapolated / 1e13, exponent_PL_lower)
+        f_max_i = np.concatenate((f_max_extrapolated, f_max_loaded_truncated))
+        m_delta_values = np.concatenate((m_delta_extrapolated, m_delta_loaded_truncated))
+                            
+        f_PBH_i_LN = constraint_Carr(mc_values, m_delta_values, f_max_i, LN, [sigma], evolved, t)      
+        data_filename_LN = data_folder + "LN_GC_%s" % constraints_names_short[i] + "_Carrs_sigma={:.1f}_approx.txt".format(sigma)
+        np.savetxt(data_filename_LN, [mc_values, f_PBH_i_LN], delimiter="\t")
+
+
 #%% Tests of the method from Bellomo et al. (2018) [1709.07467].
 from scipy.optimize import fsolve
 
@@ -165,20 +161,21 @@ def g_GC_photons(m, m_delta_values, f_max_i):
         return np.interp(m, m_delta_values, f_max_i)
     
     else:
-        return f_max_i * (m / min(m_delta_values))**(-2)
+        return f_max_i[0] * (m / min(m_delta_values))**(-2)
     
 
-def g_integral_lower(m_c, sigma):
-    return (m_c**2 / 2) * np.exp(2*sigma**2) * (erf((np.log(1e13 / m_c) + 2*sigma**2) / (np.sqrt(2) * sigma)) + 1)
+def g_integral_lower(m_c, sigma, f_max_i):
+    proportionality_constant = f_max_i[0] * np.power(1e13, -2)
+    return proportionality_constant * (m_c**2 / 2) * np.exp(2*sigma**2) * (erf((np.log(1e13 / m_c) + 2*sigma**2) / (np.sqrt(2) * sigma)) + 1)
 
 
 def g_integral(m_c, sigma, m_max, m_delta_values, f_max_i, n_steps=10000):
     """
     Integral used when calculating the equivalent mass (RHS of Eq. 2.5 or 2.6 of Bellomo et al. (2018))
     """
-    print("m_c = ", m_c)
-    print("sigma = ", sigma)
-    print("m_max = ", m_max)
+    #print("m_c = ", m_c)
+    #print("sigma = ", sigma)
+    #print("m_max = ", m_max)
     
     
     m_pbh_values = np.logspace(13, np.log10(m_max), n_steps)
@@ -186,22 +183,27 @@ def g_integral(m_c, sigma, m_max, m_delta_values, f_max_i, n_steps=10000):
     integrand_upper = LN(m_pbh_values, m_c, sigma) * g_values
     
     
-    print("np.shape(g_values) =", np.shape(g_values))
-    print("np.shape(integrand_upper) = ", np.shape(integrand_upper))
-    print("np.shape(m_pbh_values) = ", np.shape(m_pbh_values))
-    print("np.shape(g_integral output) = ", np.shape(np.trapz(integrand_upper, m_pbh_values) + g_integral_lower(m_c, sigma)))
-    print("np.shape(np.trapz(integrand_upper, m_pbh_values)) = ", np.shape(np.trapz(integrand_upper, m_pbh_values)))
-    print("g_integral_lower(m_c, sigma) = ", g_integral_lower(m_c, sigma) )
-    print("np.shape(np.sum(integrand_upper[:-1] * np.diff(m_pbh_values))) =", np.shape(np.sum(integrand_upper[:-1] * np.diff(m_pbh_values))))
-    return np.sum(integrand_upper[:-1] * np.diff(m_pbh_values)) + g_integral_lower(m_c, sigma)
+    #print("np.shape(g_values) =", np.shape(g_values))
+    #print("np.shape(integrand_upper) = ", np.shape(integrand_upper))
+    #print("np.shape(m_pbh_values) = ", np.shape(m_pbh_values))
+    #print("np.shape(g_integral output) = ", np.shape(np.trapz(integrand_upper, m_pbh_values) + g_integral_lower(m_c, sigma)))
+    #print("np.shape(np.trapz(integrand_upper, m_pbh_values)) = ", np.shape(np.trapz(integrand_upper, m_pbh_values)))
+    #print("g_integral_lower(m_c, sigma) = ", g_integral_lower(m_c, sigma) )
+    #print("np.shape(np.sum(integrand_upper[:-1] * np.diff(m_pbh_values))) =", np.shape(np.sum(integrand_upper[:-1] * np.diff(m_pbh_values))))
+    #return np.sum(integrand_upper[:-1] * np.diff(m_pbh_values)) + g_integral_lower(m_c, sigma)
 
-    #return np.trapz(integrand_upper, m_pbh_values) + g_integral_lower(m_c, sigma)
+    #print("integral (upper) =", np.trapz(integrand_upper, m_pbh_values))
+    #print("integral (lower) =", g_integral_lower(m_c, sigma, f_max_i))
+
+    return np.trapz(integrand_upper, m_pbh_values) + g_integral_lower(m_c, sigma, f_max_i)
 
 def meq_finder(m_eq, sigma, m_max, m_delta_values, f_max_i, n_steps=10000):
     """
     Function to solve for the equivalent mass.
     """
-    print("len(meq_finder output) = ", len( g_GC_photons(m_eq, m_delta_values, f_max_i) - g_integral(m_c, sigma, m_max, m_delta_values, f_max_i, n_steps)))
+    #print("len(meq_finder output) = ", len( g_GC_photons(m_eq, m_delta_values, f_max_i) - g_integral(m_c, sigma, m_max, m_delta_values, f_max_i, n_steps)))
+    #print("g_GC_photons(m_eq, m_delta_values, f_max_i) =", g_GC_photons(m_eq, m_delta_values, f_max_i))
+    #print("g_integral(m_c, sigma, m_max, m_delta_values, f_max_i, n_steps) =", g_integral(m_c, sigma, m_max, m_delta_values, f_max_i, n_steps))
     return g_GC_photons(m_eq, m_delta_values, f_max_i) - g_integral(m_c, sigma, m_max, m_delta_values, f_max_i, n_steps)
 
 def f_EMD(m_eq, m_delta_values, f_max_i):
@@ -210,13 +212,10 @@ def f_EMD(m_eq, m_delta_values, f_max_i):
 
 if "__main__" == __name__:
     
-    sigma = 0.5
-    
-    # Boolean determines whether to useFalse evolved mass function.
-    evolved = True
-    
+    sigma = 1.84859
+        
     # Boolean determines whether to use the full mass range where f_max is known or only the range where |f_max| < 1
-    all_fmax = False
+    all_fmax = True
         
     m_delta_values_loaded = np.logspace(11, 21, 1000)
     constraints_names, f_max_Isatis = load_results_Isatis(modified=True)
@@ -265,5 +264,45 @@ if "__main__" == __name__:
             m_eq_estimate = fsolve(meq_finder, m_c, args=(sigma, m_max, m_delta_values, f_max_i))[0]
             f_EMD_values.append(f_EMD(m_eq_estimate, m_delta_values, f_max_i))
   
-        data_filename_LN = data_folder + "/LN_GC_%s" % constraints_names_short[i] + "_Bellomo_sigma={:.1f}_approx.txt".format(sigma)
+        data_filename_LN = data_folder + "LN_GC_%s" % constraints_names_short[i] + "_Bellomo_sigma={:.1f}_approx.txt".format(sigma)
         np.savetxt(data_filename_LN, [mc_values, f_EMD_values], delimiter="\t")
+
+#%% Plot results for different combinations
+
+if "__main__" == __name__:
+    
+    #sigma = 0.373429
+    sigma = 1.84859   
+    fig, ax = plt.subplots(figsize=(8,8))
+    
+    for i in range(len(constraints_names)):
+        # Load and plot results for the unevolved mass functions
+        data_filename_LN_Carr_all_fmax = "./Data-tests/Carr_Bellomo_comparison/full_m_range/PL_exp_2/LN_GC_%s_Carrs_" % constraints_names_short[i] + "sigma={:.1f}_approx.txt".format(sigma)
+        data_filename_LN_Carr = "./Data-tests/Carr_Bellomo_comparison/truncated_m_range/PL_exp_2/LN_GC_%s_Carrs_" % constraints_names_short[i] + "sigma={:.1f}_approx.txt".format(sigma)
+        data_filename_LN_Bellomo_all_fmax = "./Data-tests/Carr_Bellomo_comparison/full_m_range/PL_exp_2/LN_GC_%s_Bellomo_" % constraints_names_short[i] + "sigma={:.1f}_approx.txt".format(sigma)
+        data_filename_LN_Bellomo = "./Data-tests/Carr_Bellomo_comparison/truncated_m_range/PL_exp_2/LN_GC_%s_Bellomo_" % constraints_names_short[i] + "sigma={:.1f}_approx.txt".format(sigma)
+    
+        mc_Carr_all_fmax, f_PBH_Carr_all_fmax = np.genfromtxt(data_filename_LN_Carr_all_fmax, delimiter="\t")
+        mc_Carr, f_PBH_Carr = np.genfromtxt(data_filename_LN_Carr, delimiter="\t")
+        mc_Bellomo_all_fmax, f_PBH_Bellomo_all_fmax = np.genfromtxt(data_filename_LN_Bellomo_all_fmax, delimiter="\t")
+        mc_Bellomo, f_PBH_Bellomo = np.genfromtxt(data_filename_LN_Bellomo, delimiter="\t")
+            
+        ax.plot(mc_Carr_all_fmax, f_PBH_Carr_all_fmax, linestyle="dotted", color=colors_evap[i])
+        ax.plot(mc_Carr_all_fmax, f_PBH_Carr_all_fmax, color=colors_evap[i], alpha=0.5)
+        ax.plot(mc_Bellomo, f_PBH_Bellomo, color=colors_evap[i], linestyle="None", marker="x")
+        ax.plot(mc_Bellomo_all_fmax, f_PBH_Bellomo_all_fmax, color=colors_evap[i], linestyle="None", marker="+")
+
+    ax.plot(0,0, color="k", label="Carr+: Including whole mass range", alpha=0.5)
+    ax.plot(0,0, color="k", linestyle="dotted", label="Carr+: truncated mass range")
+    ax.plot(0,0, color="k", linestyle="None", marker="+", label="Bellomo+: Including whole mass range")
+    ax.plot(0,0, color="k", linestyle="None", marker="x", label="Bellomo+: truncated mass range")
+
+    #ax.set_title("Carr et al. (2017) method")
+    ax.set_xlabel("$m_c~[\mathrm{g}]$")                
+    ax.set_ylabel("$f_\mathrm{PBH}$")
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_xlim(1e14, 1e18)
+    ax.set_ylim(1e-10, 1)
+    ax.legend(fontsize="x-small")
+    fig.tight_layout()