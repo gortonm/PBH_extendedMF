d297acbc104649f911cfb0ec7698f64b78f7d955
gortonm <matthew.gorton@nottingham.ac.uk>
2023-08-03 22:12:16 +0100

    Includes a simple working example, with no power-law extrapolation in the delta-function MF constraint.

--- v2/Carr_Bellomo_comparison.py
+++ v2/Carr_Bellomo_comparison.py
@@ -152,131 +152,48 @@ if "__main__" == __name__:
 #%% Constraints from COMPTEL, INTEGRAL, EGRET and Fermi-LAT. Approximate results obtained by using f_max as the constraint from each instrument, rather than the minimum over each energy bin.
 # Obtained using the method from Bellomo et al. (2018) [1709.07467].
 
-#from scipy.special import erf
 from scipy.optimize import fsolve
 
-def g_GC_photons(m, m_delta_values, f_max_i):
-    return np.interp(m, m_delta_values, 1/np.array(f_max_i), left=0, right=0)
-
-"""
-def g_integral_lower(m_c, sigma, f_max_i):
-    proportionality_constant = (1 / f_max_i[0]) * np.power(1/1e13, -2)
-    #return proportionality_constant * (m_c**(-2) / 2) * np.exp(2*sigma**2) * (erf((np.log(1e13 / m_c) + 2*sigma**2) / (np.sqrt(2) * sigma)) + 1)
-    return proportionality_constant * (m_c**(-2) / 2) * np.exp(2*sigma**2) * (erf((np.log(1e13 / m_c) + 2*sigma**2) / (np.sqrt(2) * sigma)) - erf((np.log(1e11 / m_c) + 2*sigma**2) / (np.sqrt(2) * sigma)))
-"""
-
-def g_integral(m_c, sigma, m_max, m_delta_values, f_max_i, n_steps=10000):
-    """
-    Integral used when calculating the equivalent mass (RHS of Eq. 2.5 or 2.6 of Bellomo et al. (2018))
-    """ 
-    m_pbh_values = np.logspace(11, np.log10(m_max), n_steps)
-    g_values = [g_GC_photons(m, m_delta_values, f_max_i) for m in m_pbh_values]
-    integrand_upper = LN(m_pbh_values, m_c, sigma) * g_values
-    
-    return np.trapz(integrand_upper, m_pbh_values)
-
-def meq_finder(m_eq, m_c, sigma, m_max, m_delta_values, f_max_i, n_steps=10000):
-    """
-    Function to solve for the equivalent mass.
-    """
-    #print("len(meq_finder output) = ", len( g_GC_photons(m_eq, m_delta_values, f_max_i) - g_integral(m_c, sigma, m_max, m_delta_values, f_max_i, n_steps)))
-    #print("g_GC_photons(m_eq, m_delta_values, f_max_i) =", g_GC_photons(m_eq, m_delta_values, f_max_i))
-    #print("g_integral(m_c, sigma, m_max, m_delta_values, f_max_i, n_steps) =", g_integral(m_c, sigma, m_max, m_delta_values, f_max_i, n_steps))
-    return g_integral(m_c, sigma, m_max, m_delta_values, f_max_i, n_steps) - g_GC_photons(m_eq, m_delta_values, f_max_i)
-
-def f_EMD(m_eq, m_delta_values, f_max_i):
-    
-    return np.interp(m_eq, m_delta_values, f_max_i, left=0, right=0)
+def m_eq_func(m, f_max_allpositive, m_delta_allpositive, m_c, sigma):
+    return np.interp(m, m_delta_allpositive, 1/f_max_allpositive) - np.trapz(LN(m_delta_allpositive, m_c, sigma) / f_max_allpositive, m_delta_allpositive)
 
 if "__main__" == __name__:
-    
     fig, ax = plt.subplots(figsize=(8,8))
-    
     sigma = 1.84859
                 
     m_delta_values_loaded = np.logspace(11, 21, 1000)
     constraints_names, f_max_Isatis = load_results_Isatis(modified=True)
     colors_evap = ["tab:orange", "tab:green", "tab:red", "tab:blue"]
-    constraints_names_short = ["COMPTEL_1107.0200", "EGRET_9811211", "Fermi-LAT_1101.1381", "INTEGRAL_1107.0200"]
-    
     mc_values = np.logspace(14, 20, 120)
-    
-    t = t_0
-    if all_fmax:
-        data_folder = "./Data-tests/Carr_Bellomo_comparison/full_m_range"
-    else:
-        data_folder = "./Data-tests/Carr_Bellomo_comparison/truncated_m_range"
-    
+
     for i in range(len(constraints_names)):
-        f_max_allpositive = []
-        m_delta_loaded_allpositive = []
-
-        for k, f_max in enumerate(f_max_Isatis[i]):
-            if f_max > 0:
-                f_max_allpositive.append(f_max)
-                m_delta_loaded_allpositive.append(m_delta_values_loaded[k])
-                    
-        f_max_i = f_max_allpositive
-        m_delta_values = m_delta_loaded_allpositive
-                             
         
-        f_EMD_values = []
-        for m_c in mc_values:
-            m_eq_estimate = fsolve(meq_finder, m_c, args=(m_c, sigma, m_max, m_delta_values, f_max_i))[0]
-            #print("M_eq = {:.3e} g".format(m_eq_estimate))
-            f_EMD_values.append(f_EMD(m_eq_estimate, m_delta_values, f_max_i))
-            #print(f_EMD(m_eq_estimate, m_delta_values, f_max_i))
-  
-        #data_filename_LN = data_folder + "LN_GC_%s" % constraints_names_short[i] + "_Bellomo_sigma={:.1f}_approx.txt".format(sigma)
-        #np.savetxt(data_filename_LN, [mc_values, f_EMD_values], delimiter="\t")
+        f_max_i = np.array(f_max_Isatis[i])
         
-        ax.plot(mc_values, f_EMD_values, color=colors_evap[i])
-        
-    ax.set_xlabel("$m_c~[\mathrm{g}]$")                
-    ax.set_ylabel("$f_\mathrm{PBH}$")
-    ax.set_xscale("log")
-    ax.set_yscale("log")
-    fig.tight_layout()
-    ax.set_xlim(1e14, 1e18)
-    ax.set_ylim(1e-10, 1)
-    fig.tight_layout()
-
-#%% Plot results for different combinations
-
-if "__main__" == __name__:
-    
-    #sigma = 0.373429
-    sigma = 1.84859   
-    fig, ax = plt.subplots(figsize=(8,8))
-    
-    for i in range(len(constraints_names)):
-        # Load and plot results for the unevolved mass functions
-        data_filename_LN_Carr_all_fmax = "./Data-tests/Carr_Bellomo_comparison/full_m_range/PL_exp_2/LN_GC_%s_Carrs_" % constraints_names_short[i] + "sigma={:.1f}_approx.txt".format(sigma)
-        data_filename_LN_Carr = "./Data-tests/Carr_Bellomo_comparison/truncated_m_range/PL_exp_2/LN_GC_%s_Carrs_" % constraints_names_short[i] + "sigma={:.1f}_approx.txt".format(sigma)
-        data_filename_LN_Bellomo_all_fmax = "./Data-tests/Carr_Bellomo_comparison/full_m_range/PL_exp_2/LN_GC_%s_Bellomo_" % constraints_names_short[i] + "sigma={:.1f}_approx.txt".format(sigma)
-        data_filename_LN_Bellomo = "./Data-tests/Carr_Bellomo_comparison/truncated_m_range/PL_exp_2/LN_GC_%s_Bellomo_" % constraints_names_short[i] + "sigma={:.1f}_approx.txt".format(sigma)
-    
-        mc_Carr_all_fmax, f_PBH_Carr_all_fmax = np.genfromtxt(data_filename_LN_Carr_all_fmax, delimiter="\t")
-        mc_Carr, f_PBH_Carr = np.genfromtxt(data_filename_LN_Carr, delimiter="\t")
-        mc_Bellomo_all_fmax, f_PBH_Bellomo_all_fmax = np.genfromtxt(data_filename_LN_Bellomo_all_fmax, delimiter="\t")
-        mc_Bellomo, f_PBH_Bellomo = np.genfromtxt(data_filename_LN_Bellomo, delimiter="\t")
+        f_max_allpositive = f_max_i[f_max_i > 0.]
+        m_delta_allpositive = m_delta_values_loaded[f_max_i > 0.]
             
-        ax.plot(mc_Carr_all_fmax, f_PBH_Carr_all_fmax, linestyle="dotted", color=colors_evap[i])
-        ax.plot(mc_Carr_all_fmax, f_PBH_Carr_all_fmax, color=colors_evap[i], alpha=0.5)
-        ax.plot(mc_Bellomo, f_PBH_Bellomo, color=colors_evap[i], linestyle="None", marker="x")
-        ax.plot(mc_Bellomo_all_fmax, f_PBH_Bellomo_all_fmax, color=colors_evap[i], linestyle="None", marker="+")
+        f_PBH_Bellomo = []
+                
+        for m_c in mc_values:
+            m_eq_estimate = fsolve(m_eq_func, m_c/100, args=(f_max_allpositive, m_delta_allpositive, m_c, sigma))
+            f_PBH_Bellomo.append(np.interp(m_eq_estimate, m_delta_allpositive, f_max_allpositive))
+            
+        ax.plot(mc_values, f_PBH_Bellomo, color=colors_evap[i])
+        
+        f_PBH_Carr = constraint_Carr(mc_values, m_delta_allpositive, f_max_allpositive, LN, params=[sigma], evolved=False)      
+        ax.plot(mc_values, f_PBH_Carr, color=colors_evap[i], linestyle="None", marker="x")
+    
+    ax.plot(0,0, color="k", label="Bellomo et al. (2018) method")
+    ax.plot(0,0, color="k", label="Carr et al. (2017) method", marker="x", linestyle="None")
 
-    ax.plot(0,0, color="k", label="Carr+: Including whole mass range", alpha=0.5)
-    ax.plot(0,0, color="k", linestyle="dotted", label="Carr+: truncated mass range")
-    ax.plot(0,0, color="k", linestyle="None", marker="+", label="Bellomo+: Including whole mass range")
-    ax.plot(0,0, color="k", linestyle="None", marker="x", label="Bellomo+: truncated mass range")
-
-    #ax.set_title("Carr et al. (2017) method")
     ax.set_xlabel("$m_c~[\mathrm{g}]$")                
     ax.set_ylabel("$f_\mathrm{PBH}$")
     ax.set_xscale("log")
     ax.set_yscale("log")
+    ax.set_title("No power law extrapolation \n Unevolved LN ($\sigma = {:.1f}$)".format(sigma))
+    ax.legend(fontsize="small")
+    fig.tight_layout()
     ax.set_xlim(1e14, 1e18)
-    ax.set_ylim(1e-10, 1)
-    ax.legend(fontsize="x-small")
+    ax.set_ylim(1e-12, 1)
     fig.tight_layout()
